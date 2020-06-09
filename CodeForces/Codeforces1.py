270555155289baa3ec44dd650ece072988a919e4
Tejas1510 <64543913+Tejas1510@users.noreply.github.com>
2020-06-09 22:18:14 +0530

    Delete Codeforces1.py

--- CodeForces/Codeforces1.py
+++
@@ -1,9 +0,0 @@
-""""""""""""""""""""""""""""""""""""""
-Name of Question:Watermelon
-Link of Question:http://codeforces.com/problemset/problem/4/A
-"""""""""""""""""""""""""""""""""""""""
-n=int(input())
-if(n%2==0 and n!=2):
-    print("YES")
-else:
-    print("NO")
